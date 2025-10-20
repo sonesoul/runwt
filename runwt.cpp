656e37221dde3ce0fdc9ecc4c365fb606dc3e5b8
sonesoul <smippy098@gmail.com>
2025-10-20 17:16:02 -0600

    add files

---
+++ runwt.cpp
@@ -0,0 +1,14 @@
+﻿#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+int WINAPI WinMain(
+	_In_ HINSTANCE, 
+	_In_opt_ HINSTANCE,
+	_In_ LPSTR args, 
+	_In_ int)
+{
+	ShellExecuteA(nullptr, "open", "wt.exe", args, nullptr, SW_SHOW);
+	return 0;
+}
